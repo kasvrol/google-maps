2b207ef7066f8772c9b4c535ccd2a753dbf03caf
ana karolina <ana.karolina@gabl.lan>
2022-02-04 17:47:27 -0300

    tentiva de requisição da api do waze

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './maps.scss';
 
 function Maps() {
-    return (
-      <div className="App">
-        <ul>
-            <li>aaaaaaaa</li>
-            <li>aaaaaaaa</li>
-            <li>aaaaaaaa</li>
-        </ul>
-      </div>
-    );
-  }
+
+  const [products, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect((zoom, lat, lng)=>{
+    fetch(`https://www.waze.com/pt-BR/live-map/directions?z=${zoom}&to=ll.${lat}%2C${lng}`, 
+    {mode: 'no-cors',
+    credentials: 'include'}).then(response => response.json()).then(data => setProduct(data))
+  }, [])
+    
+    if(loading){
+      return(
+        <h1>
+          Carregando...
+        </h1>
+      )
+    }
   
-  export default Maps;
+    return (
+      <div className="map">
+        {products(17, -25.42925552483614, -49.272003769320875)}
+      </div>
+  )
+
+}
+  
+export default Maps;
