2fdcbd6316217d2c26dcb2b8c3030faec9a58db2
ana karolina <ana.karolina@gabl.lan>
2022-02-04 11:38:12 -0300

    adição do react

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    {function App() {
+        return (
+          <div className="App">
+          </div>
+        );
+      }
+    }
+  </React.StrictMode>,
+  document.getElementById('root')
+);
